009d5f03e8f85471fd3817b004deda150676ba88
jisu kim <lerry37@naver.com>
2021-04-25 03:29:14 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const b_btn = document.querySelector(".Blue_btn");
+const y_btn = document.querySelector(".Yellow_btn");
+const p_btn = document.querySelector(".Pink_btn");
+
+const blue = document.querySelectorAll("article.Blue");
+const yellow = document.querySelectorAll("article.Yellow");
+const pink = document.querySelectorAll("article.Pink");
+
+const HIDE = "hidden";
+const SHOW = "show";
+
+function showing(show_item) {
+    show_item.forEach(e => e.classList.remove(HIDE));
+    show_item.forEach(e => e.classList.add(SHOW));
+}
+
+function hidding(hide_item) {
+    hide_item.forEach(e => e.classList.remove(SHOW));
+    hide_item.forEach(e => e.classList.add(HIDE));
+}
+
+function showBlue() {
+    hidding(yellow);
+    hidding(pink);
+    showing(blue);
+}
+
+function showYellow() {
+    hidding(blue);
+    hidding(pink);
+    showing(yellow);
+}
+
+function showPink() {
+    hidding(yellow);
+    hidding(blue);
+    showing(pink);
+}
+
+function handleSubmit() {
+    b_btn.addEventListener("click", showBlue);
+    y_btn.addEventListener("click", showYellow);
+    p_btn.addEventListener("click", showPink);
+
+}
+
+function init() {
+    handleSubmit();
+}
+
+init();
